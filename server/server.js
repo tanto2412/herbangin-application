52177186bffb6178c6c2584b73b43ee9eeaa40e4
Kristanto <kristanto@getstok.com>
2024-02-16 14:14:34 +0700

    remove expire

--- server/server.js
+++ server/server.js
@@ -26,7 +26,6 @@ const sessionOptions = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     sameSite: 'None',
     secure: true,
   },
