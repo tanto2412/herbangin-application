9174008e4f8c91fb7c21f933747a60fb3b1bff20
Kristanto <kristanto@getstok.com>
2024-02-21 22:07:12 +0700

    router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
 import LoginLayout from './pages/LoginLayout'
-import HomeLayout from './pages/HomeLayout'
 import { Routes, Route } from 'react-router-dom'
 import { ProtectedRoute, UnprotectedRoute } from './utils/ProtectedRoute'
+import MasterBarang from './pages/MasterBarang'
+import MasterSales from './pages/MasterSales'
+import HomeLayout from './pages/HomeLayout'
+import Home from './pages/Home'
+import MasterUser from './pages/MasterUser'
+import MasterPelanggan from './pages/MasterPelanggan'
+import PenerimaanBarang from './pages/PenerimaanBarang'
+import PenjualanBarang from './pages/PenjualanBarang'
+import ReturPenjualanBarang from './pages/ReturPenjualanBarang'
+import PembayaranJualBarang from './pages/PembayaranJualBarang'
+import GiroPenjualan from './pages/GiroPenjualan'
+import Laporan from './pages/Laporan'
 
 function App() {
   return (
@@ -17,7 +28,77 @@ function App() {
           <Route
             path="/home"
             element={<ProtectedRoute redirectTo="/" outlet={<HomeLayout />} />}
-          />
+          >
+            <Route
+              index
+              path=""
+              element={<ProtectedRoute redirectTo="/" outlet={<Home />} />}
+            />
+            <Route
+              path="user"
+              element={
+                <ProtectedRoute redirectTo="/" outlet={<MasterUser />} />
+              }
+            />
+            <Route
+              path="pelanggan"
+              element={
+                <ProtectedRoute redirectTo="/" outlet={<MasterPelanggan />} />
+              }
+            />
+            <Route
+              path="sales"
+              element={
+                <ProtectedRoute redirectTo="/" outlet={<MasterSales />} />
+              }
+            />
+            <Route
+              path="barang"
+              element={
+                <ProtectedRoute redirectTo="/" outlet={<MasterBarang />} />
+              }
+            />
+            <Route
+              path="penerimaan"
+              element={
+                <ProtectedRoute redirectTo="/" outlet={<PenerimaanBarang />} />
+              }
+            />
+            <Route
+              path="penjualan"
+              element={
+                <ProtectedRoute redirectTo="/" outlet={<PenjualanBarang />} />
+              }
+            />
+            <Route
+              path="retur"
+              element={
+                <ProtectedRoute
+                  redirectTo="/"
+                  outlet={<ReturPenjualanBarang />}
+                />
+              }
+            />
+            <Route
+              path="pembayaran"
+              element={
+                <ProtectedRoute
+                  redirectTo="/"
+                  outlet={<PembayaranJualBarang />}
+                />
+              }
+            />
+            <Route
+              path="giro"
+              element={
+                <ProtectedRoute redirectTo="/" outlet={<GiroPenjualan />} />
+              }
+            />
+            <Route
+              path="laporan"
+              element={<ProtectedRoute redirectTo="/" outlet={<Laporan />} />}
+            />
+          </Route>
         </Routes>
       </div>
     </>
--- client/src/components/Contents.tsx
+++ client/src/components/Contents.tsx
@@ -1,64 +1,27 @@
-import { useState } from 'react'
-import Home from '../pages/Home'
-import MasterUser from '../pages/MasterUser'
-import MasterPelanggan from '../pages/MasterPelanggan'
-import MasterSales from '../pages/MasterSales'
-import MasterBarang from '../pages/MasterBarang'
-import PenerimaanBarang from '../pages/PenerimaanBarang'
-import PenjualanBarang from '../pages/PenjualanBarang'
-import ReturPenjualanBarang from '../pages/ReturPenjualanBarang'
-import PembayaranJualBarang from '../pages/PembayaranJualBarang'
-import GiroPenjualan from '../pages/GiroPenjualan'
-import Laporan from '../pages/Laporan'
+import { NavLink, Outlet } from 'react-router-dom'
 
-const myMenu = [
-  'Home',
-  'Master User',
-  'Master Pelanggan',
-  'Master Sales',
-  'Master Barang',
-  'Penerimaan Barang',
-  'Penjualan Barang',
-  'Retur Barang',
-  'Pembayaran Jual Barang',
-  'Giro Penjualan',
-  'Laporan',
-]
+const menuMap = new Map<string, string>([
+  ['Home', ''],
+  ['Master User', 'user'],
+  ['Master Pelanggan', 'pelanggan'],
+  ['Master Sales', 'sales'],
+  ['Master Barang', 'barang'],
+  ['Penerimaan Barang', 'penerimaan'],
+  ['Penjualan Barang', 'penjualan'],
+  ['Retur Barang', 'retur'],
+  ['Pembayaran Jual Barang', 'pembayaran'],
+  ['Giro Penjualan', 'giro'],
+  ['Laporan', 'laporan'],
+])
 
-const componentsMap: Record<string, React.FC> = {
-  Home: Home,
-  'Master User': MasterUser,
-  'Master Pelanggan': MasterPelanggan,
-  'Master Sales': MasterSales,
-  'Master Barang': MasterBarang,
-  'Penerimaan Barang': PenerimaanBarang,
-  'Penjualan Barang': PenjualanBarang,
-  'Retur Barang': ReturPenjualanBarang,
-  'Pembayaran Jual Barang': PembayaranJualBarang,
-  'Giro Penjualan': GiroPenjualan,
-  Laporan: Laporan,
-}
-
-type activeMenuList = (typeof myMenu)[number]
+menuMap
 
 const Contents = () => {
-  const [activeMenu, setActiveMenu] = useState<activeMenuList>(myMenu[0])
-
-  const buttons = myMenu.map((myMenu, index) => (
-    <button
-      key={index}
-      type="button"
-      className={
-        'btn btn-outline-success rounded-0' +
-        (activeMenu == myMenu ? ' active' : '')
-      }
-      onClick={() => setActiveMenu(myMenu)}
-    >
-      {myMenu}
-    </button>
+  const buttons = [...menuMap].map(([key, value]) => (
+    <NavLink className="btn btn-outline-success rounded-0" to={value} end>
+      {key}
+    </NavLink>
   ))
-
-  const ComponentToRender = componentsMap[activeMenu]
 
   return (
     <>
@@ -71,7 +34,7 @@ const Contents = () => {
         <div className="p-2">
           <div className="tab-content">
             <div key="contentDiv" className="tab-pane fade show active">
-              {ComponentToRender && <ComponentToRender />}
+              <Outlet />
             </div>
           </div>
         </div>
