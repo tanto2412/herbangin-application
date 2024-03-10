35928775517f9a720d263436c2570b6f177d0b83
Kristanto <kristanto@getstok.com>
2024-03-10 19:03:09 +0700

    email not unique

---
+++ server/migrations/20240310120102_email_not_unique.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('customer', function (table) {
+    table.dropUnique(['email'])
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('customer', function (table) {
+    table.unique('email')
+  })
+}
